6178bc349dcf82ca48e5d756a24c1484c5c08bcb
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-06 09:17:06 +0545

    connection successful

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { connectToMongoDB } = require("./connect.js");
 const urlRoute = require("./routes/url.js");
 const app = express();
 const PORT = 8000;
+
+connectToMongoDB(
+  "mongodb+srv://guragainaayush7:uGa8zKUpLX3PtyUJ@cluster0.zmn4btw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+).then(() => console.log("Mongodb Connected"));
+
 app.use("/url", urlRoute);
 app.listen(PORT, () => console.log(`Server Started at port:${PORT}`));
